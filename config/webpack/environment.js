c850d294f6ad5b48be96a65bddbf20f5b2dd4250
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2020-07-22 21:51:10 -0300

    add bootstrap and vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+const vue = require('vue')
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jquery: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default'],
+  moment: 'moment'
+}))
+environment.loaders.prepend('vue', vue)
 module.exports = environment
