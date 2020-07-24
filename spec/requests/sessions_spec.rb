065b7fe3834cae58034e91e9ff240f27b7377346
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2020-07-24 18:09:45 -0300

    fiz session_request rspec tets

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -15,34 +15,34 @@ RSpec.describe 'Sessions', type: :request do
 
   describe 'GET /login' do
     it "render the 'login' template" do
-      get '/login'
+      get login_url
       expect(response).to be_successful
-      expect(response).to render_template('sessions/login')
+      expect(response).to render_template :login
     end
     it 'redirect to menu if account is logged in' do
       log_in_as(account, password)
-      get '/login'
-      expect(response).to redirect_to '/accounts/menu'
+      get login_url
+      expect(response).to redirect_to menu_url
     end
   end
   describe 'POST /login' do
     context 'with valid params' do
       it "redirects to 'accounts/menu'" do
-        post '/login', params: valid_params
-        expect(response).to redirect_to('/accounts/menu')
+        post login_url, params: valid_params
+        expect(response).to redirect_to menu_url
       end
     end
     context 'with invalid params' do
       it "redirects to 'login/'" do
-        post '/login', params: invalid_params
-        expect(response).to render_template('sessions/login')
+        post login_url, params: invalid_params
+        expect(response).to redirect_to login_url
       end
     end
   end
   describe 'GET /logout' do
     it 'has to destroy_account_session' do
-      get '/logout'
-      expect(response).to redirect_to '/login'
+      get logout_url
+      expect(response).to redirect_to login_url
     end
   end
 end
